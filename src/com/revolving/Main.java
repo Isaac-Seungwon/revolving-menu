1361411669642fdb5df32785e09f590d27ad49e3
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-09-20 21:11:59 +0900

    feat(member) 회원 메뉴 추가

--- src/com/revolving/Main.java
+++ src/com/revolving/Main.java
@@ -1,12 +1,97 @@
 package com.revolving;
 
 import com.revolving.data.Data;
+import com.revolving.member.MemberMenu;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-		Data.allLoad();
 		
+		System.out.println();
+		System.out.println("                  _ .-') _                                           _   .-')      _ (`-.    ('-.         .-') _ ");
+		System.out.println("		 ( (  OO) )                                         ( '.( OO )_   ( (OO  )  ( OO ).-.    ( OO ) )" );
+		System.out.println("		  \\     .'_  .-'),-----.  ,--.      ,--.      ,-.-') ,--.   ,--.)_.`     \\ / . --. /,--./ ,--,'");
+		System.out.println("		  ,`'--..._)( OO'  .-.  ' |  |.-')  |  |.-')  |  |OO)|   `.'   |(__...--''  | \\-.  \\ |   \\ |  |\\ ");
+		System.out.println("		  |  |  \\  '/   |  | |  | |  | OO ) |  | OO ) |  |  \\|         | |  /  | |.-'-'  |  ||    \\|  | )");
+		System.out.println("		  |  |   ' |\\_) |  |\\|  | |  |`-' | |  |`-' | |  |(_/|  |'.'|  | |  |_.' | \\| |_.'  ||  .     |/");
+		System.out.println("		  |  |   / :  \\ |  | |  |(|  '---.'(|  '---.',|  |_.'|  |   |  | |  .___.'  |  .-.  ||  |\\    |");
+		System.out.println("		  |  '--'  /   `'  '-'  ' |      |  |      |(_|  |   |  |   |  | |  |       |  | |  ||  | \\   | ");
+		System.out.println("		  `-------'      `-----'  `------'  `------'  `--'   `--'   `--' `--'        `--' `--'`--'  `--'" );
+		
+		Data.allLoad();
+		MemberMenu.menu();
+		
+		/*
+		
+		System.out.println();
+		System.out.println("  ;)( ;              ");
+		System.out.println("  :----:     o8Oo.   ");
+		System.out.println(" C|====| ._o8o8o8Oo_.");
+		System.out.println("  |    |  \\========/");
+		System.out.println("  `----'   `------'  ");
+
+		System.out.println();
+		System.out.println("         (           ");
+		System.out.println("          )          ");
+		System.out.println("     __..---..__     ");
+		System.out.println(" ,-='  /  |  \\  `=-. ");
+		System.out.println(":--..___________..--;");
+		System.out.println(" \\.,_____________,./ ");
+		
+		System.out.println();
+		System.out.println("        _....._           ");
+		System.out.println("    _.:`.--|--.`:._       ");
+		System.out.println("  .: .'\\o  | o /'. '.    ");
+		System.out.println(" // '.  \\ o|  /  o '.\\  ");
+		System.out.println("//'._o'. \\ |o/ o_.-'o\\\\");
+		System.out.println("|| o '-.'.\\|/.-' o   ||  ");
+		System.out.println("||--o--o-->|'             ");
+		*/
+	}
+	
+	/**
+	 * 메뉴의 주어진 옵션과 '돌아가기' 옵션을 출력하는 메소드
+	 * @param options 출력할 옵션
+	 */
+	public static void printOption(String... options) {
+		for (int i = 0; i < options.length; i++) {
+			System.out.println((i + 1) + ". " + options[i]);
+		}
+		System.out.println("0. 돌아가기");
+		Main.printLine();
+		System.out.print("번호 입력: ");
+	}
+	
+	/**
+	 * 주어진 문자열을 가운데 정렬하여 출력하는 메소드
+	 * @param text 출력할 문자열
+	 */
+	public static void printMenu(String text) {
+		String line = "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━";
+		int totalWidth = line.length() / 2; // 총 출력 폭 설정(75)
+
+		String formattedText = centerText(text, totalWidth);
+
+		Main.printLine();
+		System.out.println(formattedText);
+		Main.printLine();
+	}
+
+	/**
+	 * 주어진 문자열을 주어진 폭에 맞게 가운데 정렬하여 반환하는 메소드
+	 * @param text  가운데 정렬할 문자열
+	 * @param width 문자열을 정렬할 폭
+	 * @return 가운데 정렬된 문자열
+	 */
+	public static String centerText(String text, int width) {
+		return String.format("%" + width + "s", text);
+	}
+	
+	/**
+	 * 가운데 정렬된 선을 출력하는 메소드
+	 */
+	public static void printLine() {
+	    String line = "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━";
+	    System.out.println(line);
 	}
 }
--- src/com/revolving/member/MenuRate.java
+++ src/com/revolving/member/MenuRate.java
@@ -1,5 +1,21 @@
 package com.revolving.member;
 
+import java.util.Scanner;
+
+import com.revolving.Main;
+import com.revolving.data.RatingData;
+import com.revolving.data.object.Rating;
+
 public class MenuRate {
 
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void viewStoreReview() {
+		
+		Main.printMenu("매장 평가 열람");
+		
+		for (Rating review : RatingData.list) {
+			System.out.printf("%s %s %s %s\r\n", review.getMemberNo(), review.getStoreNo(), review.getReview(), review.getScore());
+		}
+	}
 }
