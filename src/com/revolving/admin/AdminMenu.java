2feaee9c0fd6dc8b2e1bd0ab85d9883afa5b4026
pengzer1 <pengzer1@gmail.com>
2023-09-20 21:07:46 +0900

    feat. 관리자 메뉴 추가

--- src/com/revolving/admin/AdminMenu.java
+++ src/com/revolving/admin/AdminMenu.java
@@ -1,5 +1,32 @@
 package com.revolving.admin;
 
+import java.util.Scanner;
+
+import com.revolving.Main;
+
 public class AdminMenu {
+	
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void Menu() {
+		
+		Main.printMenu("관리자 메뉴");
+		Main.printOption("메뉴 관리", "회원 정보 관리", "카테고리 관리", "사용자의 매장 및 음식 평가 관리");
+		
+		String input = scan.nextLine();
+		
+		if (input.equals("1")) {
+			// 메뉴 관리
+		} else if (input.equals("2")) {
+			// 회원 정보 관리
+		} else if (input.equals("3")) {
+			// 카테고리 관리
+		} else if (input.equals("4")) {
+			// 사용자 평가 관리
+		} else {
+			return;
+		}
+		
+	}
 
 }
