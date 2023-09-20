79516bb258465a529c79288111100976181bf058
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-09-20 22:31:11 +0900

    feat(MenuRate) 매장 평가 열람 기능 추가

--- src/com/revolving/member/MenuRate.java
+++ src/com/revolving/member/MenuRate.java
@@ -1,7 +1,6 @@
 package com.revolving.member;
 
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.Collections;
 
 import com.revolving.Main;
@@ -14,16 +13,16 @@ import com.revolving.data.object.Store;
 
 public class MenuRate {
 
-	static Scanner scan = new Scanner(System.in);
-	private static int currentIndex = 0;
-	
-	public static void viewStoreReview() {
-	
-		Main.printMenu("매장 평가 열람");
-		
-		Collections.sort(StoreData.list, (store1, store2) -> store1.getName().compareTo(store2.getName()));
+    static Scanner scan = new Scanner(System.in);
+    private static int currentIndex = 0;
+    
+    public static void viewStoreReview() {
+    
+        Main.printMenu("매장 평가 열람");
+        
+        Collections.sort(StoreData.list, (store1, store2) -> store1.getName().compareTo(store2.getName()));
 
-		int endIndex = Math.min(currentIndex + 20, RatingData.list.size());
+        int endIndex = Math.min(currentIndex + 20, RatingData.list.size());
 
         for (int i = currentIndex; i < endIndex; i++) {
             Rating review = RatingData.list.get(i);
@@ -49,18 +48,18 @@ public class MenuRate {
             
             /*
             for (int j=1; j <= Integer.parseInt(review.getScore()); j++) {
-            	System.out.print("★");
+                System.out.print("★");
             }
             System.out.println();
             */
             
             if (i != 19 && (i + 1) % 20 != 0) {
-            	System.out.println();
+                System.out.println();
             }
         }
 
         
-    	Main.printLine();
+        Main.printLine();
         if (endIndex < RatingData.list.size()) {
             System.out.println("1. 더 보기");
         }
@@ -75,90 +74,28 @@ public class MenuRate {
         
         
         
-		if (input.equals("1")) {
-			if (endIndex < RatingData.list.size()) {
+        if (input.equals("1")) {
+            if (endIndex < RatingData.list.size()) {
                 currentIndex += 20;
                 viewStoreReview();
             } else {
                 System.out.println("더 이상 리뷰가 없습니다.");
                 System.out.println("Enter를 누르면 이전 화면으로 돌아갑니다.");
-        		scan.nextLine();
+                scan.nextLine();
             }
-		} else if (input.equals("2")) {
-			if (currentIndex > 0) {
+        } else if (input.equals("2")) {
+            if (currentIndex > 0) {
                 currentIndex = Math.max(0, currentIndex - 20);
                 viewStoreReview();
             } else {
                 System.out.println("이전 화면이 없습니다.");
                 System.out.println("Enter를 누르면 이전 화면으로 돌아갑니다.");
-        		scan.nextLine();
+                scan.nextLine();
             }
-		} else {
-			System.out.println("잘못 입력하셨습니다.");
-			System.out.println("Enter를 누르면 이전 화면으로 돌아갑니다.");
-			scan.nextLine();
+        } else {
+            System.out.println("잘못 입력하셨습니다.");
+            System.out.println("Enter를 누르면 이전 화면으로 돌아갑니다.");
+            scan.nextLine();
         }
     }
 }
-
-
-/*
-package com.revolving.member;
-
-import java.util.Scanner;
-
-import com.revolving.Main;
-import com.revolving.data.MemberData;
-import com.revolving.data.RatingData;
-import com.revolving.data.StoreData;
-import com.revolving.data.object.Member;
-import com.revolving.data.object.Rating;
-import com.revolving.data.object.Store;
-
-public class MenuRate {
-
-	static Scanner scan = new Scanner(System.in);
-	
-	public static void viewStoreReview() {
-		
-		Main.printMenu("매장 평가 열람");
-
-		for (Rating review : RatingData.list) {
-			for (Member member : MemberData.list) {
-				if (member.getNo().equals(review.getMemberNo())) {
-					System.out.printf("회원명: %s\n", member.getName());
-					break;
-				}
-			}
-			for (Store store : StoreData.list) {
-				if (store.getNo().equals(review.getStoreNo())) {
-					System.out.printf("매장명: %s\n", store.getName());
-					System.out.printf("매장주소: %s\n", store.getAddress());
-					System.out.printf("매장번호: %s\n", store.getTel());
-					break;
-				}
-			}
-			System.out.printf("리뷰: %s\n", review.getReview());
-			System.out.printf("점수: %s\n", review.getScore());
-		}
-		
-		/*
-		System.out.println("[회원명][식당명][주소][리뷰][점수]");
-		for (Rating review : RatingData.list) {
-			for (Member member : MemberData.list) {
-				if (member.getNo().equals(review.getMemberNo())) {
-					System.out.printf("%s ", member.getName());
-					break;
-				}
-			}
-			for (Store store : StoreData.list) {
-				if (store.getNo().equals(review.getStoreNo())) {
-					System.out.printf("%s %s %s ", store.getName(), store.getAddress(), store.getTel());
-					break;
-				}
-			}
-			System.out.printf("%s %s\r\n", review.getReview(), review.getScore());
-		}
-	}
-}
-*/
