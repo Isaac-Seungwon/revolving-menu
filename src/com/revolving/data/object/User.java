fab9f172018747fef4d37b92f402465f7a85ab89
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-09-23 20:25:22 +0900

    feat(User) user 클래스에 로그인된 회원 정보 저장

---
+++ src/com/revolving/data/object/User.java
@@ -0,0 +1,20 @@
+package com.revolving.data.object;
+
+public class User {
+	
+	private String id;
+	private String password;
+
+	public User(String id, String password) {
+		this.id = id;
+		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+}
