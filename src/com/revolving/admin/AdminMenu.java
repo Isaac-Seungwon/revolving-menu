be93d37b074c94cd593d62872aa90abb8a8abccb
dmsalsgus <leeje971118@naver.com>
2023-09-24 12:03:12 +0900

    카테고리관리-삭제

--- src/com/revolving/admin/AdminMenu.java
+++ src/com/revolving/admin/AdminMenu.java
@@ -1,5 +1,35 @@
 package com.revolving.admin;
 
+import java.util.Scanner;
+
+import com.revolving.Main;
+
 public class AdminMenu {
+   
+   static Scanner scan = new Scanner(System.in);
+   
+   public static void Menu() {
+      
+      while(true) {
+         Main.printMenu("관리자 메뉴");
+         Main.printOption("메뉴 관리", "매장 관리", "회원 정보 관리", "카테고리 관리", "사용자의 매장 및 음식 평가 관리");
+         
+         String input = scan.nextLine();
+         
+         if (input.equals("1")) {
+            // 메뉴 관리
+         } else if (input.equals("2")) {
+            // 매장 관리
+         } else if (input.equals("3")) {
+            // 회원 정보 관리
+         } else if (input.equals("4")) {
+            CategoryManagement.checkCategory();
+         } else if (input.equals("5")) {
+            // 사용자 평가 관리
+         } else {
+            return;
+         }
+      }
+   }
 
 }
--- src/com/revolving/admin/CategoryManagement.java
+++ src/com/revolving/admin/CategoryManagement.java
@@ -1,5 +1,54 @@
 package com.revolving.admin;
 
+import com.revolving.Main;
+import com.revolving.data.CategoryData;
+import com.revolving.data.object.Category;
+
+import java.io.BufferedReader;
+import java.util.*;
+
 public class CategoryManagement {
+	/**
+	 * 카테고리 관리 클래스
+	 * 
+	 * @author lje 목적 : 관리자가 카테고리를 관리하는 클래스 기능 : 카테고리 추가, 수정, 삭제가 가능하다.
+	 */
+
+	public static void checkCategory() {
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			Main.printMenu("카테고리 관리");
+			Main.printLine();
+			Main.printOption("카테고리 추가", "카테고리 수정", "카테고리 삭제");
+			String check = sc.nextLine().trim();
+
+			if (check.equals("1")) {  //추가
+
+			} else if (check.equals("2")) {  //수정
+
+			} else if (check.equals("3")) {  //삭제
+				Main.printMenu("카테고리 삭제");
+				System.out.println("삭제할 카테고리의 번호 또는 이름을 입력하세요. ");
+				String del=sc.nextLine().trim();
+				
+				//ArrayList<Category> deleteCategory = new ArrayList<Category>();
+				
+				for(Category c : CategoryData.list) {
+					if(del.equals(c)) {
+						CategoryData.list.remove(c);
+					}
+				}
+				
+				CategoryData.save();
+				
+				
+				
+				
+			} else
+				return;
+		}
+
+	}
 
 }
