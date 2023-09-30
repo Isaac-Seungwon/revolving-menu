83b9252d6746c8811c27101144a70f91b53f5c2d
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-09-30 17:30:14 +0900

    chore(MemberMenu) 후기 작성 기능 마이페이지로 이동

--- src/com/revolving/member/MemberMenu.java
+++ src/com/revolving/member/MemberMenu.java
@@ -3,7 +3,6 @@ package com.revolving.member;
 import java.util.Scanner;
 
 import com.revolving.Main;
-import com.revolving.login.Login;
 import com.revolving.member.mypage.MyPageMenu;
 
 public class MemberMenu {
@@ -14,7 +13,7 @@ public class MemberMenu {
 		
 		while (true) {
 			Main.printMenu("회원 메뉴");
-			Main.printOption("메뉴 추천", "메뉴 목록 정렬", "후기 작성", "매장 평가 조회", "마이페이지");
+			Main.printOption("메뉴 추천", "메뉴 목록 정렬", "매장 평가 조회", "마이페이지");
 			String input = scan.nextLine().trim();
 			
 			if (input.equals("1")) {
@@ -22,10 +21,8 @@ public class MemberMenu {
 			} else if (input.equals("2")) {
 				
 			} else if (input.equals("3")) {
-				
-			} else if (input.equals("4")) {
 				MenuRate.viewStoreReview();
-			} else if (input.equals("5")) {
+			} else if (input.equals("4")) {
 				MyPageMenu.viewUserMenu();
 			} else {
 				return;
