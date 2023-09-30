1defcd98ad08aa125a8c8ce08c4bb18921a3d6ea
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-09-30 16:50:20 +0900

    feat(MyRate) 나의 매장 평가 조회, 추가, 수정, 삭제 기능 추가

--- src/com/revolving/member/mypage/MyPageMenu.java
+++ src/com/revolving/member/mypage/MyPageMenu.java
@@ -12,7 +12,7 @@ public class MyPageMenu {
 		
 		while (true) {
 			Main.printMenu("마이페이지");
-			Main.printOption("나의 추천 메뉴", "나의 매장 평가 조회", "회원 정보 수정");
+			Main.printOption("나의 추천 메뉴", "나의 매장 평가", "회원 정보 수정");
 			String input = scan.nextLine().trim();
 			
 			if (input.equals("1")) {
