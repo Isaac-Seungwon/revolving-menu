e840630c2377f214c24578210d6b8b12046d15f8
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-09-30 20:07:03 +0900

    chore(MyStoreList) 회원의 추천 메뉴 기록 조회 및 삭제 주석 수정

--- src/com/revolving/member/mypage/MyStoreList.java
+++ src/com/revolving/member/mypage/MyStoreList.java
@@ -18,18 +18,19 @@ import com.revolving.login.Login;
 
 /**
  * 나의 추천 메뉴 클래스
- * @author 이승원
  * 목적: 나의 추천 메뉴에 대한 조회, 삭제 기능을 수행한다.
  * 기능:
  * - 나의 추천 메뉴 조회: 메뉴와 방문일을 출력한다.
  * - 나의 추천 메뉴 삭제: 넘버링된 번호를 입력하여 메뉴 기록을 삭제한다.
+ * 
+ * @author 이승원
  */
 public class MyStoreList {
 
 	static Scanner scan = new Scanner(System.in);
 
 	/**
-	 * 나의 추천 메뉴 조회 및 삭제 기능을 수행하는 메서드.
+	 * 나의 추천 메뉴를 조회 및 삭제하는 메서드
 	 */
 	public static void viewMyStoreList() {
 
@@ -69,7 +70,8 @@ public class MyStoreList {
 	}
 
 	/**
-     * 회원의 추천 메뉴 기록을 조회하는 메서드.
+     * 회원의 추천 메뉴 기록을 조회하는 메서드
+     * 
      * @param member 로그인 회원
      */
 	private static void viewMyMenuRecord(Member member) {
@@ -110,7 +112,7 @@ public class MyStoreList {
 	}
 
 	/**
-     * 회원의 추천 메뉴 기록을 삭제하는 메서드.
+     * 회원의 추천 메뉴 기록을 삭제하는 메서드
      */
 	private static void deleteVisitedStoreRecord() {
 
@@ -123,22 +125,20 @@ public class MyStoreList {
 
 		// 반복자로 추천 메뉴 기록을 하나씩 가져옴
 		for (Iterator<MenuChoose> iterator = MenuChooseData.list.iterator(); iterator.hasNext();) {
-
 			MenuChoose menuChoose = iterator.next();
 
 			// 로그인한 사용자의 추천 메뉴 기록인지 확인
 			if (menuChoose.getMemberNo().equals(Login.user.getNo())) {
-
 				for (Menu menu : MenuData.list) {
 					if (menu.getNo().equals(menuChoose.getMenuNo())) {
-
+						
 						// 사용자가 선택한 번호와 리스트 번호와 일치하면 해당 추천 메뉴 기록 삭제
 						if (count == choice) {
-							
                             iterator.remove();
                             MenuChooseData.save();
 							System.out.print("선택한 메뉴 기록을 삭제했습니다.\n");
 							scan.nextLine();
+							
 							
 							// 해당 메뉴 기록에 대한 리뷰가 있으면 삭제할지 선택
 							for (Store store : StoreData.list) {
